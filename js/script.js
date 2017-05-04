14ebaaace2dd6530241b8ddb9a81e64f5c96ee4c
irinasayshi <irinagorelik@Irinas-MacBook-Pro.local>
2017-05-03 18:29:16 -0600

    Moved JS to folder

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('signUp').onclick = talkToUser;
+
+function talkToUser(){
+	event.preventDefault();
+	alert("Not Implemented!");
+	document.getElementById('header').style.backgroundColor = "orange";
+}
